f3d32cf18ce2525d3653d400f3214b65676df3b1
Willow McGinley <mcginleywillowm@gmail.com>
2025-02-08 04:13:27 -0500

    Efficiency

--- IngrKnowledge.h
+++ IngrKnowledge.h
@@ -3,26 +3,22 @@
 #include <iostream>
 
 struct IngrKnowledge {
-    bool knowRawDamage;
     bool knowRawHealing;
-    bool knowCookedDamage;
     bool knowCookedHealing;
     bool knowNumMixes;
     bool knowCookTime;
 
     IngrKnowledge()
-        : knowRawDamage(false), knowRawHealing(false), knowCookedDamage(false),
+        : knowRawHealing(false),
         knowCookedHealing(false), knowNumMixes(false), knowCookTime(false) {}
 
     IngrKnowledge(bool rawDamage, bool rawHealing, bool cookedDamage, bool cookedHealing,
         bool numMixes, bool cookTime)
-        : knowRawDamage(rawDamage), knowRawHealing(rawHealing), knowCookedDamage(cookedDamage),
+        : knowRawHealing(rawHealing),
         knowCookedHealing(cookedHealing), knowNumMixes(numMixes), knowCookTime(cookTime) {}
 
 	void printKnowledge() {
-		std::cout << "Knows raw damage: " << knowRawDamage << std::endl;
 		std::cout << "Knows raw healing: " << knowRawHealing << std::endl;
-		std::cout << "Knows cooked damage: " << knowCookedDamage << std::endl;
 		std::cout << "Knows cooked healing: " << knowCookedHealing << std::endl;
 		std::cout << "Knows number of mixes: " << knowNumMixes << std::endl;
 		std::cout << "Knows cook time: " << knowCookTime << std::endl;
--- IngrProperties.h
+++ IngrProperties.h
@@ -2,18 +2,16 @@
 #define INGRPROPERTIES_H
 
 struct IngrProperties {
-	int rawDamage;
 	int rawHealing;
-	int cookedDamage;
 	int cookedHealing;
 	int numStirs;
 	int cookTime;
 
 	IngrProperties()
-		: rawDamage(0), rawHealing(0), cookedDamage(0), cookedHealing(0), numStirs(0), cookTime(0) {}
+		: rawHealing(0), cookedHealing(0), numStirs(0), cookTime(0) {}
 
 	IngrProperties(int rawDamage, int rawHealing, int cookedDamage, int cookedHealing, int numStirs, int cookTime)
-		: rawDamage(rawDamage), rawHealing(rawHealing), cookedDamage(cookedDamage), cookedHealing(cookedHealing),
+		: rawHealing(rawHealing), cookedHealing(cookedHealing),
 		numStirs(numStirs), cookTime(cookTime) {}
 };
 
